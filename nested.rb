d842c173a153cc920cd31c85a0e7d5da66ecc3c8
Andrew Bay <daddnbay@yahoo.com>
2019-07-09 20:57:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
   programmer_hash
 end
 
@@ -102,7 +102,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-  programmer_hash[:alan_kay][:known_for][]   
+   programmer_hash[:alan_kay][:known_for] = "GUI"  
+  programmer_hash
      
 end
 
