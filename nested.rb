ab80c0bf2f097affc3efd3ceecb8f715f94a9d16
Andrew Bay <daddnbay@yahoo.com>
2019-07-09 21:01:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -120,9 +120,9 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C", "Assembly"]
+          :languages => ["C"]
         }
      }
-   programmer_hash[:dennis_ritchie][:languages][1]
+   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
